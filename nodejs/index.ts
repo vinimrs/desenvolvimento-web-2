32be93c9d01624f403304cbe2c1b4330e7c2d50b
mthsB3ssa <mthsbessac@gmail.com>
2024-01-25 16:56:25 -0300

    Criando rota e retorno dos produtos

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,10 +1,25 @@
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 const port = 3001;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+// Rota para obter a lista de produtos
+app.get("/produtos", async (req, res) => {
+  try {
+    // Leia os dados do arquivo produtos.json
+    const produtosData = await fs.readFile("products.json", "utf-8");
+    const produtos = JSON.parse(produtosData).produtos;
+
+    res.json(produtos);
+  } catch (error) {
+    console.error("Erro ao ler o arquivo de produtos:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 app.listen(port, () => {
