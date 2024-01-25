cc13830675bad52b55194acb73354ff08bae9173
vini <viniromualdo082@gmail.com>
2024-01-25 19:08:23 -0300

    feat: server/client integration

--- nodejs/app.ts
+++ nodejs/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import fs from "fs/promises";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
+
+app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -12,9 +15,10 @@ app.get("/", (req, res) => {
 app.get("/produtos", async (req, res) => {
   try {
     // Leia os dados do arquivo produtos.json
-    const produtosData = await fs.readFile("products.json", "utf-8");
-    const produtos = JSON.parse(produtosData).produtos;
+    const produtosData = await fs.readFile("./products.json", "utf-8");
+    const produtos = JSON.parse(produtosData);
 
+    // Envie a lista de produtos como resposta
     res.json(produtos);
   } catch (error) {
     console.error("Erro ao ler o arquivo de produtos:", error);
